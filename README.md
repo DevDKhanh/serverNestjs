0c17de60762db1c683ae7c230f1ca0e264804c05
DevDKhanh <duykhanh06.work@gmail.com>
2022-01-01 17:32:57 +0700

    init server

---
+++ README.md
@@ -0,0 +1,3 @@
+<p>Account Cloundinary</p>
+<p>username :phongnd@vmodev.com</p>
+<p>password:Phongit_1995</p>
---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+
+import { AuthModule } from './modules/auth/auth.module';
+@Module({
+  imports: [
+    SharedModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get('MYSQL_HOST'),
+        port: config.get<number>('MYSQL_PORT'),
+        database: config.get<string>('MYSQL_DATABASE'),
+        username: config.get<string>('MYSQL_USERNAME'),
+        password: config.get<string>('MYSQL_PASSWORD'),
+        synchronize: config.get<boolean>('MYSQL_SYNC'),
+        autoLoadEntities: true,
+        entities: ['dist/**/*.entity.{ts,js}'],
+        migrationsTableName: 'migration',
+        migrations: ['src/migration/*.ts'],
+        retryAttempts: 5,
+      }),
+    }),
+    AuthModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
